bc5128860ddd02edd36dd6aef2e3d7d5a64da4d7
Mihails Strasuns <mihails.strasuns.contractor@sociomantic.com>
2016-12-05 19:30:29 +0200

    Don't use D-APT for compiler downloads
    It doesn't store old compiler versions

--- docker/dmd.sh
+++ docker/dmd.sh
@@ -4,17 +4,9 @@
 #   - dmd-bin package version (default: latest)
 set -xe
 
-dmd_version=""
-test -n "$1" &&
-        dmd_version="=$1"
+dmd_version="2.071.2"
 
-# Install D-APT packages
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EBCF975E5BA24D5E && \
-        wget http://master.dl.sourceforge.net/project/d-apt/files/d-apt.list \
-            -O /etc/apt/sources.list.d/d-apt.list
+apt -y install xdg-utils libcurl3
 
-# Make sure our packages list is updated
-apt-get update
-
-# Install dmd-bin
-apt-get install -y dmd-bin$dmd_version
+wget -O dmd.deb http://downloads.dlang.org/releases/2.x/${dmd_version}/dmd_${dmd_version}-0_amd64.deb
+dpkg -i dmd.deb
