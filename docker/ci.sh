e848e9722b2f893e3c091890ce6cf3f6b68c028e
Leandro Lucarella <leandro.lucarella@sociomantic.com>
2017-06-19 18:33:05 +0200

    Travis: Use beaver to build
    * submodules/beaver ()...v0.1.1(2c6caef) (1 commits)
      > Bugfix: Change TRAVIS_SLUG to TRAVIS_REPO_SLUG

--- docker/ci.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -xe
-
-export F=production
-make -r all
-make -r test
-make -r pkg
--- docker/travis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -xe
-
-git submodule foreach --recursive git submodule deinit --force --all
-./docker/ci.sh
